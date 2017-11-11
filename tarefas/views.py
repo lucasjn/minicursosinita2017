a6d36211d9f4a840fded8cb1e735e85ca3306323
LucasJN <lucas.j.nascimento@hotmail.com>
2017-11-10 21:31:41 -0300

    atualizando nome 123

--- tarefas/views.py
+++ tarefas/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponseRedirect, HttpResponse
 from tarefas.models import Tarefas
 
 def hello(request):
-    return HttpResponse('Olá mundo! Lucas JN')
+    return HttpResponse('Olá mundo! Lucas ')
 
 def adicionar(nome, descricao):
     """Essa função salva a tarefa no banco de dados"""
